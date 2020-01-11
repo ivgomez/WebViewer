52a1b666c9535bf320fbf6603ffef08f4104a090
Vanessa <laurav.suarezg@utadeo.edu.co>
2020-01-11 00:04:36 -0500

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+      <WebView
+        source={{ uri: 'https://www.gestionarplus.com' }}
+        style={{ marginTop: 20 }}
+      />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
